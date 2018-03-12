3a8096c3b947ddcc3f505167a47e5ed4c0d85b9f
uorai <ftorpedo7730@gmail.com>
2018-03-13 01:06:14 +0900

    花粉量の取得できた

--- PollenAlarm.py
+++ PollenAlarm.py
@@ -4,26 +4,33 @@ import urllib.parse
 import urllib.request
 from bs4 import BeautifulSoup
 
+# 定数
 POLLEN_URL="https://kafun.yahoo.co.jp/weather/%s"
-CITY_CODE="14" # KANAGAWA
+CITY_CODE="14" # 神奈川
 
+# 花粉情報取得
 def get_pollen_info():
     try:
         url = POLLEN_URL % CITY_CODE
         html = urllib.request.urlopen(url)
         soup = BeautifulSoup(html, "html.parser")
-        span = soup.find_all("div")
-        hisan_yohou = ""
+
+        # htmlタグ dd でのタグを取得
+        span = soup.find_all("dd")
+
+        # 花粉量
+        pollen_data = ""
+
         for tag in span:
             try:
-                string_ = tag.get("class").pop(0)
-                if string_ in "z-contents__hisanyohou":
-                    hisan_yohou = tag.string_
+                tag_class = tag.get("class").pop(0)
+                if tag_class in "dataBox__pollenData":
+                    pollen_data = tag.find("span", class_="text").string
                     break
             except:
                 pass
         
-        print(hisan_yohou)
+        print(pollen_data)
     except Exception as ex:
         print("Exception Error: ", ex)
         sys.exit(1)
